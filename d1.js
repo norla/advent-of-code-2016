513268a6b9023b5b532955398ed3a5178edfe678
Mattias Norlander <mattias.norlander@expressen.se>
2019-11-14 16:14:04 +0100

    d1 complete

--- d1.js
+++ d1.js
@@ -1,25 +1,11 @@
 "use strict";
 const fs = require("fs");
 const _ = require("lodash");
-/*
-You're airdropped near Easter Bunny Headquarters in a city somewhere. "Near", unfortunately, is as close as you can get -
-the instructions on the Easter Bunny Recruiting Document the Elves intercepted start here, and nobody had time to work them out further.
 
-The Document indicates that you should start at the given coordinates (where you just landed) and face North.
-Then, follow the provided sequence: either turn left (L) or right (R) 90 degrees, then walk forward the given number of blocks, ending at a new intersection.
-
-There's no time to follow such ridiculous instructions on foot, though, so you take a moment and work out the destination.
-Given that you can only walk on the street grid of the city, how far is the shortest path to the destination?
-
-For example:
-
-Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
-R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
-R5, L5, R5, R3 leaves you 12 blocks away.
-How many blocks away is Easter Bunny HQ?
-*/
-
-const input = fs.readFileSync("./input/d1.txt").toString();
+const input = fs
+      .readFileSync("./input/d1.txt", "utf8")
+      .split(", ")
+      .map((s) => ({ turn: s.slice(0, 1), len: s.slice(1) }));
 
 const directionMovements = {
   "N": {xd: 0, yd: 1},
@@ -27,30 +13,32 @@ const directionMovements = {
   "S": {xd: 0, yd: -1},
   "W": {xd: -1, yd: 0}
 }
-const directions = Object.keys(directionMovements); 
-let x = 0, y = 0;
-let direction = "N";
-let visited = [];
-let firstVisitedTwice;
-input.split(", ").forEach((step) => {
-  const turn = step.slice(0, 1);
-  const len = Number(step.slice(1));
-  const directionIndex = directions.indexOf(direction);
-  let nextDirectionIndex = (directionIndex + directions.length + (turn === "L" ? -1 : 1)) % directions.length;
-  direction = directions[nextDirectionIndex];
-  const xd = directionMovements[direction].xd * len;
-  const yd = directionMovements[direction].yd * len;
-  const visit = (vX, vY) => {
-    const coord = `${vX} ${vY}`;
-    if (!firstVisitedTwice && visited[coord]) firstVisitedTwice = {x: vX, y: vY};
-    else visited[coord] = true; 
-  }
-  _.range(x, x + xd).forEach((xStep) => visit(xStep, y));
-  _.range(y, y + yd).forEach((yStep) => visit(x, yStep));
-  x += xd;
-  y += yd;
-  //console.log(x, y, visited)
-});
+const directions = Object.keys(directionMovements);
 
-console.log("Solution 1:", Math.abs(x) + Math.abs(y));
-console.log("Solution 2:", Math.abs(firstVisitedTwice.x) + Math.abs(firstVisitedTwice.y));
+function solution1({direction, x, y, trail}, {turn, len}) {
+  const directionIndex = directions.indexOf(direction);
+  const nextDirectionIndex = (directionIndex + directions.length + (turn === "L" ? -1 : 1)) % directions.length;
+  const nextDirection = directions[nextDirectionIndex];
+  const xd = directionMovements[nextDirection].xd * len;
+  const yd = directionMovements[nextDirection].yd * len;
+  return {direction: nextDirection, x: x + xd, y: y + yd, trail: trail.concat({x, y})};
+}
+
+function solution2({lastX, lastY, visited, firstVisitedTwice}, {x, y}) {
+  const visit = (vX, vY) => {
+    const coord = {x: vX, y: vY};
+    const coordStr = JSON.stringify(coord);
+    if (!firstVisitedTwice && visited[coordStr]) firstVisitedTwice = coord;
+    else visited[coordStr] = true;
+  }
+  _.range(lastX, x).forEach((xStep) => visit(xStep, y));
+  _.range(lastY, y).forEach((yStep) => visit(x, yStep));
+  return {lastX: x, lastY: y, visited, firstVisitedTwice};
+};
+
+const start = {x: 0, y: 0, direction: "N", trail: []};
+const s1 = input.reduce(solution1, start);
+const s2 = s1.trail.reduce(solution2, {lastX: 0, lastY: 0, visited: new Map(), firstVisitedTwice: null});
+
+console.log("Solution 1:", Math.abs(s1.x) + Math.abs(s1.y));
+console.log("Solution 2:", Math.abs(s2.firstVisitedTwice.x) + Math.abs(s2.firstVisitedTwice.y));
